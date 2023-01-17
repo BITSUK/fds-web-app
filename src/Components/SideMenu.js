3f923cf337b6a1315990b0a0dab9155bea2507c2
BITSUK <2022cfse006@wilp.bits-pilani.ac.in>
2023-01-17 14:37:40 +0530

    border-colour adjustments

--- src/Components/SideMenu.js
+++ src/Components/SideMenu.js
@@ -5,7 +5,7 @@ function SideMenu(){
         <>
 			<div className="column left-nav" id="BML">
 				<div className="vertical-menu">
-					<a id="register-btn" href="#">Register / Login</a>
+					<a id="register-btn" href="#">Login</a>
 					<a href="#">Order Food</a>
 				</div>
 			</div>
