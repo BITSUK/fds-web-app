f5c67df79c3d897b27751c11b9e59ff0864120a1
BITSUK <2022cfse006@wilp.bits-pilani.ac.in>
2023-01-21 21:30:03 +0530

    worked on registration form, minor improvements

--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -5,11 +5,17 @@ import {Link} from "react-router-dom";
 export default function Registration() {
     
     const handleRegistration = (event) => {
-        alert("Resigter button clicked");        
+        if (document.getElementById("chkTerms").checked == false) {
+            alert("Please accept terms and conditions");  
+            return;
+        } else {
+            alert("Resigter button clicked");  
+        }
+              
     }
 
     const showHideRest = (event) => {
-        if (document.getElementById("radioRestaurant").checked) { 
+        if (document.getElementById("chkRestaurant").checked) { 
             document.getElementById("restaurant-details").style.display = "block";
         } else {
             document.getElementById("restaurant-details").style.display = "none";
@@ -25,10 +31,10 @@ export default function Registration() {
                 <input type="text" className="form-control" id="regFormName" placeholder="Name"/>
             </div>
             <div className="reg-form-components">
-				<input type="checkbox" id="radioCustomer" name="role-type" value="Customer" defaultChecked/> &nbsp;
-                <label htmlFor="radioCustomer">Customer</label>	&nbsp;
-				<input type="checkbox" id="radioRestaurant" name="role-type" value="Restaurant" onClick={showHideRest}/> &nbsp;
-                <label htmlFor="radioRestaurant">Restaurant</label>			
+				<input type="checkbox" id="chkCustomer" name="role-type" value="Customer" defaultChecked/> &nbsp;
+                <label htmlFor="chkCustomer">Customer</label>	&nbsp;
+				<input type="checkbox" id="chkRestaurant" name="role-type" value="Restaurant" onClick={showHideRest}/> &nbsp;
+                <label htmlFor="chkRestaurant">Restaurant</label>			
 			</div>
             
             <div className="reg-form-components">
@@ -68,30 +74,30 @@ export default function Registration() {
                 </div>
 
                 <div className="reg-form-components">
-                    <input type="checkbox" id="radioVegRest" name="restaurant-type" value="Veg" defaultChecked/> &nbsp;
-                    <label htmlFor="radioVegRest">Veg</label>	&nbsp;
-                    <input type="checkbox" id="radioNonVegRest" name="restaurant-type" value="Non-Veg"/> &nbsp;
-                    <label htmlFor="radioNonVegRest">Non-Veg</label>	&nbsp;		
-                    <input type="checkbox" id="radioFastFNonVegRest" name="restaurant-type" value="FastFood"/> &nbsp;
-                    <label htmlFor="radioFastFNonVegRest">Fast Food</label>			
+                    <input type="checkbox" id="chkVegRest" name="restaurant-type" value="Veg" defaultChecked/> &nbsp;
+                    <label htmlFor="chkVegRest">Veg</label>	&nbsp;
+                    <input type="checkbox" id="chkNonVegRest" name="restaurant-type" value="Non-Veg"/> &nbsp;
+                    <label htmlFor="chkNonVegRest">Non-Veg</label>	&nbsp;		
+                    <input type="checkbox" id="chkFastFNonVegRest" name="restaurant-type" value="FastFood"/> &nbsp;
+                    <label htmlFor="chkFastFNonVegRest">Fast Food</label>			
 			    </div>                
 
                 <div className="reg-form-components"> 
                     <label>Operational Days: </label><br/>
-                    <input type="checkbox" id="radioMon" name="restaurant-type" value="Mon" defaultChecked/> &nbsp;
-                    <label htmlFor="radioMon">Mon</label>&nbsp;
-                    <input type="checkbox" id="radioTue" name="restaurant-type" value="Tue" defaultChecked/> &nbsp;
-                    <label htmlFor="radioTue">Tue</label>&nbsp;
-                    <input type="checkbox" id="radioWed" name="restaurant-type" value="Wed" defaultChecked/> &nbsp;
-                    <label htmlFor="radioWed">Wed</label>&nbsp;
-                    <input type="checkbox" id="radioThu" name="restaurant-type" value="Thu" defaultChecked/> &nbsp;
-                    <label htmlFor="radioThu">Thu</label>&nbsp;
-                    <input type="checkbox" id="radioFri" name="restaurant-type" value="Fri" defaultChecked/> &nbsp;
-                    <label htmlFor="radioFri">Fri</label>&nbsp;
-                    <input type="checkbox" id="radioSat" name="restaurant-type" value="Sat" defaultChecked/> &nbsp;
-                    <label htmlFor="radioSat">Sat</label>&nbsp;
-                    <input type="checkbox" id="radioSun" name="restaurant-type" value="Sun" defaultChecked/> &nbsp;
-                    <label htmlFor="radioSun">Sun</label>&nbsp;
+                    <input type="checkbox" id="chkMon" name="restaurant-type" value="Mon" defaultChecked/> &nbsp;
+                    <label htmlFor="chkMon">Mon</label>&nbsp;
+                    <input type="checkbox" id="chkTue" name="restaurant-type" value="Tue" defaultChecked/> &nbsp;
+                    <label htmlFor="chkTue">Tue</label>&nbsp;
+                    <input type="checkbox" id="chkWed" name="restaurant-type" value="Wed" defaultChecked/> &nbsp;
+                    <label htmlFor="chkWed">Wed</label>&nbsp;
+                    <input type="checkbox" id="chkThu" name="restaurant-type" value="Thu" defaultChecked/> &nbsp;
+                    <label htmlFor="chkThu">Thu</label>&nbsp;
+                    <input type="checkbox" id="chkFri" name="restaurant-type" value="Fri" defaultChecked/> &nbsp;
+                    <label htmlFor="chkFri">Fri</label>&nbsp;
+                    <input type="checkbox" id="chkSat" name="restaurant-type" value="Sat" defaultChecked/> &nbsp;
+                    <label htmlFor="chkSat">Sat</label>&nbsp;
+                    <input type="checkbox" id="chkSun" name="restaurant-type" value="Sun" defaultChecked/> &nbsp;
+                    <label htmlFor="chkSun">Sun</label>&nbsp;
 			    </div>
 
                 <div className="reg-form-components">
@@ -100,6 +106,12 @@ export default function Registration() {
                 </div>
 
             </div>
+
+            <div className="reg-form-components"> 
+                    <input type="checkbox" id="chkTerms" name="terms-and-conditions" value="TC"/> &nbsp;
+                    <label htmlFor="chkTerms">I accept all terms and conditions.*</label>&nbsp;
+            </div>
+
             <div className="reg-form-components">
                 <Link to="#" className="btn btn-primary" role="button" onClick={handleRegistration}>Register</Link>
                 &nbsp;&nbsp;
